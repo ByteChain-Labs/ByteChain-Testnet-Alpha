753c7b8fda23d8c1735364b7e7d914ad51aaf9aa
nifemibosun <nifemibosun70@gmail.com>
2025-01-29 03:59:19 +0100

    Tested some gen. comps. like -Blockchain      -add_new_tx()      -addr_bal      -mine_block() -Account      -tested account creation capability -Transaction -TxPlaceHolder

---
+++ src/gen_test.ts
@@ -0,0 +1,70 @@
+import Transaction from "./core/transaction";
+import BlockChain from "./core/blockchain";
+import Account from "./accounts/account";
+import { TxPlaceHolder } from "./utils/core_constants";
+
+const bc = new BlockChain();
+
+const print = (...data: any): void => {
+    console.log(...data);
+}
+
+const user1 = new Account();
+const user2 = new Account();
+
+
+bc.addr_bal.set(user1.blockchain_addr, 500);
+
+
+print("User 1: ", bc.addr_bal.get(user1.blockchain_addr))
+print("User 2: ", bc.addr_bal.get(user2.blockchain_addr))
+
+
+// A transaction for user1 -> user2
+const tx1_ph: TxPlaceHolder = { 
+    amount: 300, 
+    sender: user1.blockchain_addr, 
+    recipient: user2.blockchain_addr,
+    comment: "This is a test tx."
+}
+
+const tx1  = new Transaction(
+    300, 
+    user1.blockchain_addr, 
+    user2.blockchain_addr, 
+    user1.sign_tx(tx1_ph, user1.priv_key),
+    "This is a test tx."
+);
+print(tx1)
+
+
+bc.add_new_tx(tx1, user1.pub_key);
+bc.mine_block(user2.blockchain_addr);
+
+print("User 1: ", bc.addr_bal.get(user1.blockchain_addr))
+print("User 2: ", bc.addr_bal.get(user2.blockchain_addr))
+
+
+
+// A transaction for user2 -> user1
+const tx2_ph: TxPlaceHolder = { 
+    amount: 24, 
+    sender: user2.blockchain_addr, 
+    recipient: user1.blockchain_addr 
+}
+
+const tx2  = new Transaction(
+    24, 
+    user2.blockchain_addr, 
+    user1.blockchain_addr, 
+    user2.sign_tx(tx2_ph, user2.priv_key),
+    "This is a test tx."
+);
+print(tx2)
+
+
+bc.add_new_tx(tx2, user2.pub_key);
+bc.mine_block(user2.blockchain_addr);
+
+print("User 1: ", bc.addr_bal.get(user1.blockchain_addr))
+print("User 2: ", bc.addr_bal.get(user2.blockchain_addr))
