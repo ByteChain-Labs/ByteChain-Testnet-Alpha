9c189e7e653df2df430af30f898bad8720e293d5
Nifemi Bosun <nifemibosun70@gmail.com>
2025-06-20 10:23:33 -0700

    Refactor transaction handling and blockchain initialization; update transaction signature verification and block properties

--- src/core/block.ts
+++ src/core/block.ts
@@ -8,27 +8,29 @@ class Block {
     block_header: BlockHeader;
     transactions: Transaction[];
 
-    constructor(block_height: number, timestamp: number, prev_block_hash: string, transactions: Transaction[]) {
+    constructor(block_height: number, difficulty: number, prev_block_hash: string, transactions: Transaction[]) {
         this.block_header = {
             nonce: 0,
             block_height: block_height,
-            timestamp: timestamp,
+            timestamp: 0,
             merkleroot: "",
             prev_block_hash: prev_block_hash,
             block_hash: "",
+            difficulty: difficulty,
         };
         this.transactions = transactions;
     }
 
-    set_block_props(difficulty: number) {
+    set_block_props() {
         try {
             this.block_header.merkleroot = calc_merkleroot<Transaction>(this.transactions);
-            const { nonce, block_height, timestamp, merkleroot,  prev_block_hash } = this.block_header;
+            const { nonce, block_height, timestamp, difficulty, merkleroot,  prev_block_hash } = this.block_header;
 
-            const block_data_str = `${nonce}${block_height}${timestamp}${merkleroot}${prev_block_hash}${this.transactions}`;
+            const block_data_str = `${nonce}${block_height}${timestamp}${difficulty}${merkleroot}${prev_block_hash}${this.transactions}`;
 
             const { n_nonce, hash } = proof_of_work(block_data_str, difficulty);
 
+            this.block_header.timestamp = Date.now();
             this.block_header.nonce = n_nonce;
             this.block_header.block_hash = hash;
         } catch (err) {
