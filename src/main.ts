6e0425d0b9dbe854b9d3e54272951d530cd40039
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-08-29 00:15:09 +0100

    Integrated socketIO

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,34 @@
 import BCNode from "./core/node.js";
 import { print } from "./utils/constants.js";
-
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const bc_node = new BCNode();
+bc_node.app.use(cors({ origin: "*" }));
+
+const server = http.createServer(bc_node.app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+bc_node.io = io;
 
 async function main() {
-    await bc_node.start();
+  await bc_node.start();
 
-    setInterval(() => {
-        bc_node.pubBlock()
-    }, 2000);
+  server.listen(3000, () => {});
+
+  setInterval(() => {
+    bc_node.pubBlock();
+  }, 2000);
 }
 
-main().catch(err => {
-    console.error(`Failed to start ByteChain Node: ${err}`)
-    process.exit(1);
+main().catch((err) => {
+  console.error(`Failed to start ByteChain Node: ${err}`);
+  process.exit(1);
 });
 
-
-process.on('SIGINT', async () => {
-    print('SIGINT received, stopping node...');
-    await bc_node.stop();
-    process.exit(0);
+process.on("SIGINT", async () => {
+  print("SIGINT received, stopping node...");
+  await bc_node.stop();
+  process.exit(0);
 });
